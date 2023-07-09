daaa33c6d1ea36683a6cd8c0eb0a8ceecad52d43
dkamundi <davekamundi@gmail.com>
2023-07-09 05:42:30 -0700

    0. Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,24 +8,45 @@ if ! command -v nginx >/dev/null 2>&1; then
 fi
 
 # Create necessary folders
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
+web_static_dir="/data/web_static"
+releases_dir="$web_static_dir/releases"
+shared_dir="$web_static_dir/shared"
+test_dir="$releases_dir/test"
+index_file="$test_dir/index.html"
 
-# Create a fake HTML file
-sudo echo "Fake content" | sudo tee /data/web_static/releases/test/index.html >/dev/null
+mkdir -p "$web_static_dir" "$releases_dir" "$shared_dir" "$test_dir"
+touch "$index_file"
+echo "Hello, world!" > "$index_file"
 
-# Create or recreate symbolic link
-sudo rm -rf /data/web_static/current
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+# Create or recreate the symbolic link
+symbolic_link="/data/web_static/current"
+if [ -L "$symbolic_link" ]; then
+    rm -f "$symbolic_link"
+fi
+ln -s "$test_dir" "$symbolic_link"
+
+# Give ownership of the /data/ folder to the ubuntu user and group recursively
+chown -R ubuntu:ubuntu /data/
 
 # Set ownership of /data/ folder to ubuntu user and group recursively
 sudo chown -R ubuntu:ubuntu /data/
 
 # Update Nginx configuration
-sudo sed -i '/hbnb_static/ { s|^.*$|        alias /data/web_static/current/;| }' /etc/nginx/sites-available/default
+config_file="/etc/nginx/sites-available/default"
+config_content=$(cat <<EOM
+server {
+    listen 80;
+    listen [::]:80;
+
+    location /hbnb_static {
+        alias $symbolic_link/;
+        index index.html;
+    }
+}
+EOM
+)
+echo "$config_content" > "$config_file"
 
 # Restart Nginx
-sudo service nginx restart
-
-exit 0
+service nginx restart
 
