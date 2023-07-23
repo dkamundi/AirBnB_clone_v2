130f1c7057e449b6624a6b89cc5ff0f9a01f6db1
dkamundi <davekamundi@gmail.com>
2023-07-23 11:27:41 -0700

    9. Cities by states

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Start a Flask web application to display states and their cities."""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_session(exception):
+    """Remove the current SQLAlchemy Session."""
+    storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """Display a HTML page with states and their cities."""
+    states = storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
