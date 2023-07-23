52d467ff6adbd9d7eb2b1486943665e04bb9f2da
dkamundi <davekamundi@gmail.com>
2023-07-23 11:22:19 -0700

    8. List of states

--- models/state.py
+++ models/state.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models.city import City
+from os import getenv
+import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+from models import storage
 
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', cascade='all, delete-orphan', backref='state')
-    
-    if models.storage_t != "db":
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', cascade='all, delete-orphan', backref='state')
+    else:
+        name = ""
+
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def  cities(self):
             """Getter attribute cities that returns the list of City instances with state_id
